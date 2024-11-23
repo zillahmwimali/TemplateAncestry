eb9c7bdb42430536e627858107825dcf09c63398
Zillah Mwimali <zillahmwimmali@gmail.com>
2024-11-23 15:08:30 +0300

    format code

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+  # Defines the root path route ("/")
+  root "home#index"
+
 end
